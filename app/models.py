d846ebf923770fa8dab661685fff7800252349b6
abhinavmabhi <dexmarketing192018@gmail.com>
2025-08-29 13:06:26 +0530

    first

---
+++ app/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Project(models.Model):
+    image = models.ImageField(upload_to="project_image/")
+    PROJECT_TYPES = [
+        ('Residential', 'Residential'),
+        ('Commercial', 'Commercial'),
+        ('Industrial', 'Industrial'),
+    ]
+    description = models.CharField(max_length=250)
+    project_type = models.CharField(max_length=20, choices=PROJECT_TYPES)
+    customer_name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    square_feet = models.PositiveIntegerField(null=True, blank=True)
+
+    # New fields for key features
+    feature1 = models.CharField(max_length=255, default="Premium Quality Materials")
+    feature2 = models.CharField(max_length=255, default="Energy Efficient")
+    feature3 = models.CharField(max_length=255, default="Modern Design Concepts")
+    feature4 = models.CharField(max_length=255, default="Customizable Options")
+
+
+    def __str__(self):
+        return f"{self.customer_name} - {self.project_type}"
+
+
+
+class Images(models.Model):
+    title=models.CharField(max_length=100)
+    image=models.ImageField(upload_to="gallery_image/")
---
+++ app/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render,get_object_or_404
+from .models import Project,Images
+# Create your views here.
+
+def home(request):
+    projects=Images.objects.order_by("-id")[:6]
+    return render(request, 'home.html',{"projects":projects})
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    services=Project.objects.all()
+    return render(request, 'services.html',{"services":services}) 
+
+def service_details(request, id): 
+    service = get_object_or_404(Project, id=id)
+
+    similar_services = Project.objects.filter(project_type=service.project_type).exclude(id=id)
+    return render(request, "service_details.html", {
+        'service': service,
+        'services': similar_services
+    })
+
+
+def gallery(request):
+    images=Images.objects.all()
+    return render(request, 'gallery.html',{"images":images})
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
